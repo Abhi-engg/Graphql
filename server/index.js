d34c36af03f99723836280f21ec87e8810067e60
Abhi-engg <abhijeetyadav33xb@gmail.com>
2025-07-12 21:08:25 +0530

    commit kardiya

--- server/index.js
+++ server/index.js
@@ -1,35 +1,34 @@
 const express = require('express')
 const { ApolloServer} = require("@apollo/server")
-const {expressMiddleware} = require("@apollo/server/express4")
+const { expressMiddleware } = require("@apollo/server/express4")
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
-async function startserver() {
+async function startServer() {
     const app = express();
     const server = new ApolloServer({
-        typeDefs:`
-            type Todo{
-            id: ID!
-            title: String!
-            completed: Boolean
-        }
+        typeDefs: `
+            type Todo {
+                id: ID!
+                title: String!
+                completed: Boolean
+            }
         
-        type Query {
-            getTodos: [Todo]
-        }
-
+            type Query {
+                getTodos: [Todo]
+            }
         `,
         resolvers: {},
     });
 
-    app.use(bodyParser.json());
+    app.use(express.json());
     app.use(cors());
 
-    await server.start()
+    await server.start();
 
     app.use("/graphql", expressMiddleware(server))
 
     app.listen(8000, () => console.log("Server Started at PORT 8000"));
 }
 
-startserver()
+startServer();
