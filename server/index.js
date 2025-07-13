c6f0a6552c248360a01bf096f72b93d06810e4c3
Abhi-engg <abhijeetyadav33xb@gmail.com>
2025-07-13 12:30:48 +0530

    react-graphql

--- server/index.js
+++ server/index.js
@@ -1,27 +1,54 @@
 const express = require('express')
-const { ApolloServer} = require("@apollo/server")
 const { expressMiddleware } = require("@apollo/server/express4")
+const { ApolloServer } = require("@apollo/server")
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const  axios  = require('axios');
 
 async function startServer() {
     const app = express();
     const server = new ApolloServer({
         typeDefs: `
+            type User{
+                id: ID!
+                name: String!
+                username: String!
+                email: String!
+                phone: String!
+                website: String!}    
+        
             type Todo {
                 id: ID!
                 title: String!
                 completed: Boolean
+                user: User
             }
         
             type Query {
                 getTodos: [Todo]
+                getAllUsers: [User]
+                getUser(id: ID!): User
             }
         `,
-        resolvers: {},
+        resolvers: {
+            Todo: {
+                user: async (todo) => (await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`))
+                .data,
+            },
+            
+            Query: {
+                getTodos:  async () =>  
+                    (await axios.get('https://jsonplaceholder.typicode.com/todos')).data,
+                getAllUsers: async () =>
+                    (await axios.get("https://jsonplaceholder.typicode.com/users")).data,
+                getUser: async (parent, {id} ) =>
+                    (await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`))
+                .data,
+            },
+        },
     });
 
-    app.use(express.json());
+    app.use(bodyParser.json());
     app.use(cors());
 
     await server.start();
